a8cf8bd1ca111b12abade322ee44dadfbd93099d
Maycon Douglas <70534369+maykkk1@users.noreply.github.com>
2022-07-22 15:27:16 -0300

    test

--- cypress/integration/backend.spec.js
+++ cypress/integration/backend.spec.js
@@ -9,5 +9,6 @@ describe('Testando o backend', () => {
             url: 'https://barrigarest.wcaquino.me/signin', 
             body: {email: "maycon", senha: "12345", redirecionar: false}
         }).its('body.token').should('not.to.be.empty')
+        console.log('test')
     })
 })
